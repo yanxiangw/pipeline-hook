57b3cdc0b3466d1870991d41410766c831dabf01
Yanxiang Wang <tauruswyx@gmail.com>
2018-06-02 17:21:54 +1000

    Update hook listener to use Slack and Pipleline instead

--- hook.py
+++ hook.py
@@ -1,36 +1,17 @@
 import os
-import util
+from pipeline import Pipeline
+from slack import Slack
 
-def slack_message(detail, time):
-    title = f"{detail['action']} on {detail['stage']} has {detail['state']}"
-    return {
-        "attachments": [
-            {
-                "pretext": "Update from AWS CodePipeline",
-                "title": title,
-                "color": "good" if detail["state"] == "SUCCEEDED" else "danger",
-                "fields": [
-                    {
-                        "title": "Version",
-                        "value": util.get_version(detail["execution-id"])
-                    }
-                ],
-                "footer": os.environ["APP_NAME"],
-                "footer_icon": os.environ["FAVICON_URL"]
-            }
-        ]
-    }
+def is_relavant(detail):
+  return detail["state"] == "FAILED" or detail["stage"] in ["Production", "Staging"]
 
-def process(event):
-    status, body = 200, "Slack message is sent"
-    detail, time = event["detail"], event["time"]
-    if detail["state"] == "FAILED" or detail["stage"] in ["Production", "Staging"]:
-        body = util.send(slack_message(detail, time))
-    else:
-        print("Other event received:", event)
-        body = "Irrelevant event received"
-    return status, body
+def process(detail):
+  slack = Slack(os.environ["APP_NAME"], os.environ["FAVICON_URL"])
+  pipeline = Pipeline(os.environ['PIPELINE_NAME'])
+  message = slack.build_message(pipeline.revision(detail["execution-id"]), os.environ['PIPELINE_NAME'], detail)
+  return slack.send(os.environ["SLACK_WEBHOOK_URL"], message)
 
 def listen(event, context):
-    status, body = process(event)
-    return { "statusCode": status, "body": body }
+  detail = event["detail"]
+  body = process(detail) if is_relavant(detail) else "Irrelevant event received"
+  return { "statusCode": 200, "body": body }
